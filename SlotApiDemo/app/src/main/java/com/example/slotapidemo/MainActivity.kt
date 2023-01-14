4cdd060407b7779b4b0e2c5cb157fb4c32ed38bb
김재희 <jaehee.kim@42dot.ai>
2023-01-14 23:11:01 +0900

    SlotApi 추가

--- SlotApiDemo/app/src/main/java/com/example/slotapidemo/MainActivity.kt
+++ SlotApiDemo/app/src/main/java/com/example/slotapidemo/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.slotapidemo
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.slotapidemo.ui.theme.SlotApiDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +25,9 @@ class MainActivity : ComponentActivity() {
             SlotApiDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    SlotDemo {
+                        SlotButton()
+                    }
                 }
             }
         }
@@ -27,14 +35,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SlotButton() {
+    val context: Context = LocalContext.current
+    Button(onClick = { Toast.makeText(context, "click", Toast.LENGTH_SHORT).show() }) {
+        Text(text = "중간에 Composable 넣고 사용자 버튼 인터페이스 제공")
+    }
+}
+
+@Composable
+fun SlotDemo(slotComposable: @Composable () -> Unit) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Top Text")
+        Spacer(modifier = Modifier.height(30.dp))
+        slotComposable()
+        Spacer(modifier = Modifier.height(30.dp))
+        Text(text = "Bottom Text")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SlotApiDemoTheme {
-        Greeting("Android")
+        SlotDemo(slotComposable = { SlotButton() })
     }
 }
