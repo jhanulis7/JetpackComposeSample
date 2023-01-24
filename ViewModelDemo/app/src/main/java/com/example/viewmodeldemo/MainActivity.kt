76be6ef9591b019e160f46bab5702a8f3be4b3a0
김재희 <jaehee.kim@42dot.ai>
2023-01-24 19:44:44 +0900

    viewmodel compose

--- ViewModelDemo/app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
+++ ViewModelDemo/app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.viewmodeldemo
 
+import android.graphics.drawable.PaintDrawable
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.viewmodeldemo.ui.theme.ViewModelDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ScreenSetup()
                 }
             }
         }
@@ -30,14 +40,129 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ScreenSetup(
+    //viewModel 을 singleTone 하지 않고, 인자를 받지 않으면 매번 새로운 뷰모델 생성 하게 됨
+    //이런 경우, hilt 싱글톤 injection 하던가, MainActivity 에서 viewModel 받도록 한다.
+    //또는 자식 composable(subScreen) 에 viewModel 상태와 핸들러를 전달한다. 이번 예제는 이걸로 한다.
+    viewModel: TempViewModel = TempViewModel()
+) {
+    ////Log.d("ViewModel", "ScreenSetup() isFahrenheit: ${viewModel.isFahrenheit}")
+
+    MainScreen(
+        isFahrenheit = viewModel.isFahrenheit,
+        result = viewModel.result,
+        convertTemp = { viewModel.convertTemp(it) },
+        switchChange = { viewModel.switchChange() }
+    )
+}
+
+@Composable
+fun MainScreen(
+    isFahrenheit: Boolean,
+    result: String,
+    convertTemp: (String) -> Unit,
+    switchChange: () -> Unit
+) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var textState by remember { mutableStateOf("") }
+        //Log.d("ViewModel", "subScreen() Enter!! textState:$textState")
+
+        val onTextChange = { text: String ->
+            //Log.d("ViewModel", "subScreen() onTextChange: text:$text")
+            textState = text
+        }
+
+        //Log.d("ViewModel", "subScreen() isFahrenheit: $isFahrenheit")
+
+        Text("Temperature Converter",
+            modifier = Modifier.padding(20.dp),
+            style = MaterialTheme.typography.h4
+        )
+
+        InputRow(
+            isFahrenheit = isFahrenheit,
+            textState = textState,
+            onSwitchChange = switchChange,
+            onTextChange = onTextChange
+        )
+
+        //Log.d("ViewModel", "subScreen() : result:$result")
+        Text(
+            text = result,
+            modifier = Modifier.padding(20.dp)
+        )
+
+        //Log.d("ViewModel", "subScreen() : textState:$textState")
+        Button(onClick = { convertTemp(textState) }) {
+            Text(
+                text = "Convert Temperature",
+                modifier = Modifier.padding(20.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun InputRow(
+    isFahrenheit: Boolean,
+    textState: String,
+    onSwitchChange: () -> Unit,
+    onTextChange: (String) -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Switch(
+            checked = isFahrenheit,
+            onCheckedChange = { onSwitchChange() }
+        )
+
+        OutlinedTextField(
+            value = textState,
+            onValueChange = {
+                onTextChange(it)
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            singleLine = true,
+            label = { Text("Enter Temperature") },
+            modifier = Modifier.padding(10.dp),
+            textStyle = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp
+            ),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_baseline_ac_unit_24),
+                    contentDescription = "frost",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+        )
+
+        Crossfade(
+            targetState = isFahrenheit,
+            animationSpec = tween(2000)
+        ) { visible ->
+            when (visible) {
+                true -> Text(text = "\u2109", style = MaterialTheme.typography.h4)
+                false -> Text(text = "\u2103", style = MaterialTheme.typography.h4)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DefaultPreview(viewModel: TempViewModel = TempViewModel()) {
     ViewModelDemoTheme {
-        Greeting("Android")
+        MainScreen(
+            isFahrenheit = viewModel.isFahrenheit,
+            result = viewModel.result,
+            convertTemp = { viewModel.convertTemp(it) },
+            switchChange = { viewModel.switchChange() }
+        )
     }
 }
---
+++ ViewModelDemo/app/src/main/java/com/example/viewmodeldemo/TempViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.viewmodeldemo
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import kotlin.math.roundToInt
+
+class TempViewModel {
+    // Composable func 에서는 아래와 같이
+    // - var sliderPosition by remember { mutableStateOf(20f) }
+    // - val checked = remember { mutableStateOf(defaultChecked) }
+    var isFahrenheit by mutableStateOf(true)
+    var result by mutableStateOf("")
+
+    fun convertTemp(temp: String) {
+        result = try {
+            val tempInt = temp.toInt()
+            if (isFahrenheit) {
+                ((tempInt - 32) * 0.5556).roundToInt().toString()
+            } else {
+                ((tempInt * 1.8) +32).roundToInt().toString()
+            }
+        } catch (e: java.lang.Exception) {
+            "Invalid Entry"
+        }
+        Log.d("ViewModel", "covertTemp() temp:$temp to result:$result")
+    }
+
+    fun switchChange() {
+        //Log.d("ViewModel", "switchChange() isFahrenheit:$isFahrenheit to ${!isFahrenheit}")
+        isFahrenheit = !isFahrenheit
+    }
+}
